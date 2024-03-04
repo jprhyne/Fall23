600bf67b32fcf0373f68e0e3ffd8bbccaa94e9c6
Johnathan Rhyne <rhynejohnathan@gmail.com>
2024-03-04 00:42:35 -0700

    merging

--- org2r/cTesters/overallTest.c
+++ org2r/cTesters/overallTest.c
@@ -9,8 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <cblas.h>
-#include <lapacke.h>
 
 double computePerform_refL(double m, double n, double k, double elapsed_refL)
 {
