9300a12296a05643cbc710fd018daeac667b036f
Johnathan Rhyne <rhynejohnathan@gmail.com>
2024-03-21 08:54:45 -0600

    lowering dimensions to not attempt to allocate too much memory

--- orgqr/timingFiles/time_dlarfb.sh
+++ orgqr/timingFiles/time_dlarfb.sh
@@ -3,13 +3,13 @@
 # Fix n and k
 # n is a decently sized number of columns, and k is the blocksize. We are sticking with
 # 32 in this as we are concerned with performance inside dorgqr for dlarfb
-for (( m=50000; m<= 500000; m+=20000 ))
+for (( m=50000; m<= 100000; m+=20000 ))
 do 
     echo "m=$m"
     echo "Testing file ran 10 times"
     for (( l=1; l<=10; l+=1 ))
     do
         # n is this small so that we can run it with our innefficient timing method.
-        ./timeDlarfb.exe -m $m -n 10000 -k 32
+        ./timeDlarfb.exe -m $m -n 1000 -k 32
     done
 done
