734665740aa4cf3abbc3c0bb53c40f0b7b3fd2db
Johnathan Rhyne <rhynejohnathan@gmail.com>
2024-04-18 16:14:45 -0600

    updating qr timing files

--- orgqr/timingFiles/timeDorgqr.c
+++ orgqr/timingFiles/timeDorgqr.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <math.h>
 
-#ifdef USE_MKL_BLAS
-    #include <mkl_cblas.h>
-#else
-    #include <cblas.h>
-#endif
-
-#ifdef USE_MKL_LAPACK
-    #include <mkl_lapacke.h>
-#else
-    #include <lapacke.h>
-#endif
+double myNorm(int m, int n, double *A, int lda)
+{
+    double ret = 0.0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            double tmp = A[i + j * lda];
+            ret += tmp*tmp;
+        }
+    }
+    return sqrt(ret);
+}
 
 int main(int argc, char **argv) {
 
@@ -24,7 +25,7 @@ int main(int argc, char **argv) {
     double normA;
     double elapsed_refL, perform_refL;
     struct timeval tp;
-    int i, j;
+    int i, j, version;
 
     char aChar = 'A';
     char fChar = 'F';
@@ -50,6 +51,7 @@ int main(int argc, char **argv) {
     nb = 3; // Choose a default nb value that is NOT a factor of k
     lda = -1;
     ldq = -1;
+    version = -1; // Default to the system version (vendor usually)
     // Flag that helps facilitate testing with the driver.c file
     bool errorsOnly = false;
     // Flag that helps facilitate timing with the time.sh file
@@ -86,21 +88,30 @@ int main(int argc, char **argv) {
         if( strcmp( *(argv + i), "-t") == 0) {
             timesOnly  = true;
         }
+        if( strcmp( *(argv + i), "-v") == 0) {
+            version = atoi( *(argv + i + 1) );
+            i++;
+        }
     }
 
     if( lda < 0 ) lda = m;
     if( ldq < 0 ) ldq = m;
-    if( k > n) k = n;
+    bool useKR = (version != 1 && version != 2);
+    if( k > n || useKR) k = n;
 
     if (!errorsOnly && !timesOnly) {
-        printf("dgeqrf dgorgqr LAPACK | ");
+        if (useKR) 
+            printf("dgeqrf dlarft_ut dorgkr | ");
+        else
+            printf("dgeqrf dlarft_ut dorgqr | ");
+        printf("version = %4d, ", version);
         printf("m = %4d, ",    m);
         printf("n = %4d, ",    n);
         printf("k = %4d, ",    k);
         printf("nb = %4d, ",    nb);
         printf("lda = %4d, ",lda);
-        printf("ldq = %4d, ",ldq);
-        printf("             ");
+        printf("ldq = %4d",ldq);
+        printf("\n");
     }
 
     A  = (double *) calloc(lda * k,sizeof(double));
@@ -114,7 +125,7 @@ int main(int argc, char **argv) {
         *(Q + i) = (double)rand() / (double)(RAND_MAX) - 0.5e+00;
 
     dlacpy_(&aChar, &m, &k, A, &lda, As, &lda, dummy);
-    normA = dlange_(&fChar, &m, &k, A, &lda, work, dummy);
+    normA = myNorm(m, k, A, lda);
     //info  = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, k, A, lda, As, lda );
     //normA = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, k, A, lda, work );
 
@@ -124,26 +135,23 @@ int main(int argc, char **argv) {
     dgeqrf_(&m, &k, A, &lda, tau, work, &negOne, &info);
     //LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, m, k, A, lda, tau, work, -1 ); 
     lwork = ((int) work[0]);
-    //my_dorgqr_(&m, &n, &k, A, &lda, tau, work, &negOne, &info);
-    dorgqr_(&m, &n, &k, A, &lda, tau, work, &negOne, &info);
+    my_dorgqr_v0_(&m, &n, &k, Q, &ldq, tau, work, &negOne, &info);
     //LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, m, n, k, A, lda, tau, work, -1 );
     if (lwork < ((int) work[0])) lwork = ((int) work[0]); 
     free( work );
+    if (lwork < n) lwork = n;
+    // determining what function to call based on the value of 'version'
     work = (double *) malloc( lwork * sizeof(double));
 
-    dgeqrf_(&m, &k, A, &lda, tau, work, &lwork, &info);
-    //LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, m, k, A, lda, tau, work, lwork ); 
+    dgeqrf_(&m, &k, A, &ldq, tau, work, &lwork, &info);
+    // Stop the timer so that we do not time copying over A into Q
+
     dlacpy_(&aChar, &m, &k, A, &lda, Q, &ldq, dummy);
-    //LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, k, A, lda, Q, ldq );
+
+    // Call the correct routine
     gettimeofday(&tp, NULL);
     elapsed_refL=-((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
-
-    //LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, m, n, k, Q, ldq, tau, work, lwork );
-    // Directly calling the fortran function dorgqr
-    dorgqr_(&m, &n, &k, Q,&ldq, tau, work, &lwork, &info);
-    // Directly calling my fortran function my_dorgqr
-    //my_dorgqr_(&m, &n, &k, Q, &ldq, tau, work, &lwork, &info);
-
+    my_dorgqr_v8_(&m, &n, &k, Q, &ldq, tau, work, &lwork, &info);
     gettimeofday(&tp, NULL);
     elapsed_refL+=((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
 
@@ -159,7 +167,7 @@ int main(int argc, char **argv) {
     //bl1_dsyrk_blas(CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n);
     dsyrk_(&uChar, &tChar, &n, &m, &one, Q, &ldq, &dNegOne, work, &n, dummy, dummy);
     //cblas_dsyrk( CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n );
-    norm_orth_1 = dlange_(&fChar, &n, &n, work, &n, NULL, dummy);
+    norm_orth_1 = myNorm(n, n, work, n);
     //norm_orth_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', n, n, work, n, NULL );
     free( work );
 
@@ -171,18 +179,134 @@ int main(int argc, char **argv) {
     for(i = 0; i < m; ++i)
         for(j = 0; j < k; ++j)
             work[ i+j*m ] -= As[ i+j*lda ];
-    norm_repres_1 = dlange_(&fChar, &m, &k, work, &m, NULL, dummy);
+    norm_repres_1 = myNorm(m, k, work, m);
     //norm_repres_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, k, work, m, NULL );
     norm_repres_1 = norm_repres_1 / normA;
     free( work );
 
     if (!errorsOnly && !timesOnly) {
+        printf("time = %f   GFlop/sec = %f", elapsed_refL, perform_refL);
+        printf("repres  = %5.1e    ortho = %5.1e ", norm_repres_1, norm_orth_1);
+    } else if (errorsOnly && !timesOnly) {
+        printf("New:%10.10e %10.10e", norm_repres_1, norm_orth_1);
+    } else {
+        printf("New:%10.10e:%10.10e", elapsed_refL, perform_refL);
+    }
+
+    printf("\n");
+    // Now we do a similar process as above but dgeqrt3 instead of dgeqrf
+    if (!errorsOnly && !timesOnly) {
+        if (useKR)
+            printf("dgeqrt3 dorgkr\n");
+        else
+            printf("dgeqrt3 dorgqr\n");
+    }
+    dlacpy_(&aChar, &m, &k, As, &lda, Q, &ldq, dummy);
+
+    work = (double *) malloc(k * k * sizeof(double));
+
+
+    dgeqrt3_(&m, &k, Q, &ldq, work, &k, &info);
+
+    free(work);
+    work = (double *) malloc(lwork * sizeof(double));
+    gettimeofday(&tp, NULL);
+    elapsed_refL=-((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
+
+    my_dorgqr_v0_(&m, &n, &k, Q, &ldq, tau, work, &lwork, &info);
+
+    gettimeofday(&tp, NULL);
+    elapsed_refL+=((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
+    // fix later. This was from timing everything
+    perform_refL = (4.0e+00*(double) m*(double)n*(double)k -2.0e+00*(double)m*(double)k*(double)k - 2.0e+00*(double)n*(double)k*(double)k + 4.0e+00/3.0e+00 *(double)k*(double)k*(double)k)/elapsed_refL /1.0e+9;
+    
+    free( work );
+    work  = (double *) malloc(n * n * sizeof(double));
+    dlaset_(&aChar, &n, &n, &zero, &one, work, &n, dummy);
+    //info  = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', n, n, (0e+00), (1e+00), work, n );
+    //bl1_dsyrk_blas(CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n);
+    dsyrk_(&uChar, &tChar, &n, &m, &one, Q, &ldq, &dNegOne, work, &n, dummy, dummy);
+    //cblas_dsyrk( CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n );
+    norm_orth_1 = myNorm(n, n, work, n);
+    //norm_orth_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', n, n, work, n, NULL );
+    free( work );
+
+    work  = (double *) malloc(m * k * sizeof(double));
+    dlacpy_(&aChar, &m, &k, Q, &ldq, work, &m, dummy);
+    //info  = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, k, Q, ldq, work, m );
+    dtrmm_(&rChar, &uChar, &nChar, &nChar, &m, &k, &one, A, &lda, work, &m, dummy, dummy, dummy, dummy);
+    //cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, m, k, (1.0e+00), A, lda, work, m );
+    for(i = 0; i < m; ++i)
+        for(j = 0; j < k; ++j)
+            work[ i+j*m ] -= As[ i+j*lda ];
+    norm_repres_1 = myNorm(m, k, work, m);
+    //norm_repres_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, k, work, m, NULL );
+    norm_repres_1 = norm_repres_1 / normA;
+    if (!errorsOnly && !timesOnly) {
         printf("| time = %f   GFlop/sec = %f", elapsed_refL, perform_refL);
         printf("| repres  = %5.1e    ortho = %5.1e ", norm_repres_1, norm_orth_1);
     } else if (errorsOnly && !timesOnly) {
-        printf("%10.10e %10.10e", norm_repres_1, norm_orth_1);
+        printf("REF:%10.10e %10.10e", norm_repres_1, norm_orth_1);
     } else {
-        printf("%10.10e:%10.10e", elapsed_refL, perform_refL);
+        printf("REF:%10.10e:%10.10e", elapsed_refL, perform_refL);
+    }
+    printf("\n");
+
+    // only have 3 calls in two of our test cases
+    // We now print a normal execution stats
+    if (!errorsOnly && !timesOnly) {
+        printf("dgeqrf dorgqr\n");
+    }
+    dlacpy_(&aChar, &m, &k, As, &lda, Q, &ldq, dummy);
+
+    free(work);
+    work = (double *) malloc(lwork * sizeof(double));
+
+
+    dgeqrf_(&m, &k, Q, &lda, tau, work, &lwork, &info);
+
+    gettimeofday(&tp, NULL);
+    elapsed_refL=-((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
+
+    dorgqr_(&m, &n, &k, Q, &ldq, tau, work, &lwork, &info);
+
+
+    gettimeofday(&tp, NULL);
+    elapsed_refL+=((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
+    // fix later. This was from timing everything
+    perform_refL = (4.0e+00*(double) m*(double)n*(double)k -2.0e+00*(double)m*(double)k*(double)k - 2.0e+00*(double)n*(double)k*(double)k + 4.0e+00/3.0e+00 *(double)k*(double)k*(double)k)/elapsed_refL /1.0e+9;
+    
+    free( work );
+    work  = (double *) malloc(n * n * sizeof(double));
+    dlaset_(&aChar, &n, &n, &zero, &one, work, &n, dummy);
+    //info  = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', n, n, (0e+00), (1e+00), work, n );
+    //bl1_dsyrk_blas(CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n);
+    dsyrk_(&uChar, &tChar, &n, &m, &one, Q, &ldq, &dNegOne, work, &n, dummy, dummy);
+    //cblas_dsyrk( CblasColMajor, CblasUpper, CblasTrans, n, m, 1.0e+00, Q, ldq, -1.0e+00, work, n );
+    norm_orth_1 = myNorm(n, n, work, n);
+    //norm_orth_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', n, n, work, n, NULL );
+    free( work );
+
+    work  = (double *) malloc(m * k * sizeof(double));
+    dlacpy_(&aChar, &m, &k, Q, &ldq, work, &m, dummy);
+    //info  = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, k, Q, ldq, work, m );
+    dtrmm_(&rChar, &uChar, &nChar, &nChar, &m, &k, &one, A, &lda, work, &m, dummy, dummy, dummy, dummy);
+    //cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, m, k, (1.0e+00), A, lda, work, m );
+    for(i = 0; i < m; ++i)
+        for(j = 0; j < k; ++j)
+            work[ i+j*m ] -= As[ i+j*lda ];
+    norm_repres_1 = myNorm(m, k, work, m);
+    //norm_repres_1 = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, k, work, m, NULL );
+    norm_repres_1 = norm_repres_1 / normA;
+    free( work );
+
+    if (!errorsOnly && !timesOnly) {
+        printf("time = %f   GFlop/sec = %f", elapsed_refL, perform_refL);
+        printf("repres  = %5.1e    ortho = %5.1e ", norm_repres_1, norm_orth_1);
+    } else if (errorsOnly && !timesOnly) {
+        printf("OPT:%10.10e %10.10e", norm_repres_1, norm_orth_1);
+    } else {
+        printf("OPT:%10.10e:%10.10e", elapsed_refL, perform_refL);
     }
 
     printf("\n");
